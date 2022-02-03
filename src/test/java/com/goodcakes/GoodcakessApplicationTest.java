9bd68a988c2ef530baed81841137e5f7a8e36bb0
Kawis77 <krzysztofsiwak77@gmail.com>
2022-02-03 23:11:41 +0100

    models

---
+++ src/test/java/com/goodcakes/GoodcakessApplicationTest.java
@@ -0,0 +1,14 @@
+package com.goodcakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodcakessApplicationTest {
+
+        @Test
+        public void contextLoads() {
+        }
+
+    }
+
