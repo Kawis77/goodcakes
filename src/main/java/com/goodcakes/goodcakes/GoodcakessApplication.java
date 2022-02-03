4ee4fc81e726bdbc0daa94a98636aacaf97ea9d6
Kawis77 <krzysztofsiwak77@gmail.com>
2022-02-03 22:54:39 +0100

    Initial commit

---
+++ src/main/java/com/goodcakes/goodcakes/GoodcakessApplication.java
@@ -0,0 +1,13 @@
+package com.goodcakes.goodcakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodcakessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodcakessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodcakes/goodcakes/GoodcakessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodcakes.goodcakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodcakessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
